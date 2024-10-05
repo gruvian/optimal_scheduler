87baca18550582cf2b95de2b6284240a85026f04
gruvian <katjamomcilov00@gmail.com>
2024-10-05 23:57:23 +0200

    Fixed relative paths at runtime

--- greedy_algorithm.py
+++ greedy_algorithm.py
@@ -6,12 +6,8 @@ from operator import itemgetter
 
 
 def resource_path(relative_path):
-    """ PyInstaller paths """
-    try:
-        base_path = sys._MEIPASS
-    except AttributeError:
-        base_path = os.path.dirname(__file__)
-
+    """ Get absolute path to resource, works for both development and PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
 
 
--- install.sh
+++ install.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 
-BASE_PATH=$(pwd)
-EXECUTABLE_PATH="$BASE_PATH/dist/optimal_scheduler"
-ICON_PATH="$BASE_PATH/icon.png"
+
+EXECUTABLE_DIR="./dist/optimal_scheduler"
 DESKTOP_FILE_PATH="$HOME/.local/share/applications/OptimalScheduler.desktop"
 INSTALL_DIR="$HOME/.local/share/OptimalScheduler"
 
-if [ ! -f "$EXECUTABLE_PATH" ]; then
-    echo "Error: Executable not found at $EXECUTABLE_PATH"
-    exit 1
-fi
-
-if [ ! -f "$ICON_PATH" ]; then
-    echo "Error: Icon not found at $ICON_PATH"
-    exit 1
-fi
 
 mkdir -p "$INSTALL_DIR"
-cp "$EXECUTABLE_PATH" "$INSTALL_DIR"
-cp "$ICON_PATH" "$INSTALL_DIR"
 
-cat > "$DESKTOP_FILE_PATH" <<EOL
+
+if [ -d "$EXECUTABLE_DIR" ]; then
+    echo "Copying files to installation directory..."
+    cp -r "$EXECUTABLE_DIR/"* "$INSTALL_DIR"
+
+    
+    chmod +x "$INSTALL_DIR/optimal_scheduler"
+
+    
+    cat > "$DESKTOP_FILE_PATH" <<EOL
 [Desktop Entry]
 Name=Optimal Scheduler
 Exec=$INSTALL_DIR/optimal_scheduler
-Icon=$INSTALL_DIR/icon.png
+Icon=$INSTALL_DIR/logoLight.png
 Type=Application
 Terminal=false
 Categories=Utility;
 EOL
 
-chmod +x "$DESKTOP_FILE_PATH"
+    
+    chmod +x "$DESKTOP_FILE_PATH"
 
-echo "Launching Optimal Scheduler..."
-$INSTALL_DIR/optimal_scheduler
+    echo "Launching Optimal Scheduler..."
+    "$INSTALL_DIR/optimal_scheduler"
 
-echo "Installation complete."
+    echo "Installation complete."
+else
+    echo "Error: Executable directory not found at $EXECUTABLE_DIR"
+    exit 1
+fi
