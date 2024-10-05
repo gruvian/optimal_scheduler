24ddf93225eddf9434067c1ace2706cd6163a794
gruvian <katjamomcilov00@gmail.com>
2024-10-05 07:01:25 +0200

    Reverted changes

--- install.sh
+++ install.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 
-BASE_PATH=$(pwd)
-EXECUTABLE_PATH="$BASE_PATH/dist/optimal_scheduler"
-ICON_PATH="$BASE_PATH/icon.png"
+EXECUTABLE_PATH="./dist/optimal_scheduler"
+ICON_PATH="./icon.png"
 DESKTOP_FILE_PATH="$HOME/.local/share/applications/OptimalScheduler.desktop"
 INSTALL_DIR="$HOME/.local/share/OptimalScheduler"
 
-if [ ! -f "$EXECUTABLE_PATH" ]; then
-    echo "Error: Executable not found at $EXECUTABLE_PATH"
-    exit 1
-fi
-
-if [ ! -f "$ICON_PATH" ]; then
-    echo "Error: Icon not found at $ICON_PATH"
-    exit 1
-fi
-
 mkdir -p "$INSTALL_DIR"
+
 cp "$EXECUTABLE_PATH" "$INSTALL_DIR"
 cp "$ICON_PATH" "$INSTALL_DIR"
 
+#debian .desktop file
 cat > "$DESKTOP_FILE_PATH" <<EOL
 [Desktop Entry]
 Name=Optimal Scheduler
@@ -30,8 +21,8 @@ Terminal=false
 Categories=Utility;
 EOL
 
+
 chmod +x "$DESKTOP_FILE_PATH"
-chmod +x "$INSTALL_DIR/optimal_scheduler"
 
 echo "Launching Optimal Scheduler..."
 $INSTALL_DIR/optimal_scheduler
