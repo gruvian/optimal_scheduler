b45abba1b16849d60e617d3bc6489c434549b167
gruvian <katjamomcilov00@gmail.com>
2024-10-05 06:46:23 +0200

    Changed path

--- install.sh
+++ install.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
 
-# Define paths
-EXECUTABLE_PATH="./dist/optimal_scheduler"
-ICON_PATH="./icon.png"
+BASE_PATH=$(pwd)/optimal_scheduler
+
+EXECUTABLE_PATH="$BASE_PATH/dist/optimal_scheduler"
+ICON_PATH="$BASE_PATH/icon.png"
 DESKTOP_FILE_PATH="$HOME/.local/share/applications/OptimalScheduler.desktop"
 INSTALL_DIR="$HOME/.local/share/OptimalScheduler"
+
 
 mkdir -p "$INSTALL_DIR"
 
 cp "$EXECUTABLE_PATH" "$INSTALL_DIR"
 cp "$ICON_PATH" "$INSTALL_DIR"
 
-#debian .desktop file
+# debian .desktop
 cat > "$DESKTOP_FILE_PATH" <<EOL
 [Desktop Entry]
 Name=Optimal Scheduler
-Exec=$INSTALL_DIR/optimal_scheduler
+Exec=$INSTALL_DIR/dist/optimal_scheduler
 Icon=$INSTALL_DIR/icon.png
 Type=Application
 Terminal=false
 Categories=Utility;
 EOL
-
 
 chmod +x "$DESKTOP_FILE_PATH"
 
